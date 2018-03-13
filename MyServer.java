00ab16382c71be456bb41d13141e403e7cd31c83
NILNASASIDHARAN <36188687+NILNASASIDHARAN@users.noreply.github.com>
2018-03-13 09:13:22 +0000

    Update MyServer.java

--- MyServer.java
+++ MyServer.java
@@ -4,4 +4,6 @@
     public static void main(String[] args){  
     try{  
     ServerSocket ss=new ServerSocket(6666);  
-    Socket s=ss.accept();//establishes connection   
+    Socket s=ss.accept();//establishes connection  
+    DataInputStream dis=new DataInputStream(s.getInputStream());  
+    String  str=(String)dis.readUTF();  
