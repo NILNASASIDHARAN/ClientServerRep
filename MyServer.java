2bac14741a320f6cae20d7c63b374cba075fa756
NILNASASIDHARAN <36188687+NILNASASIDHARAN@users.noreply.github.com>
2018-03-13 09:11:56 +0000

    Update MyServer.java

--- MyServer.java
+++ MyServer.java
@@ -4,4 +4,5 @@
     public static void main(String[] args){  
     try{  
     ServerSocket ss=new ServerSocket(6666);  
-    Socket s=ss.accept();//establishes connection   
+    Socket s=ss.accept();//establishes connection  
+    DataInputStream dis=new DataInputStream(s.getInputStream());  
